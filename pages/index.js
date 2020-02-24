11237a333c2581c7533cffd60afd3bd4099fc849
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2020-02-24 20:17:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div>
       <p>Hello from Git Sourceless</p>
-      <p>This should de-dupe</p>
+      <p>This should de-dupe 234</p>
     </div>
   )
 }
