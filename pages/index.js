cba4340a618d83f575a331bc2e2c892b60d01f05
IgorKlopov <igor@klopov.com>
2020-04-04 15:26:05 +0300

    Update .deploy

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <div>
-      <p>Hello from Git Sourceless</p>
-      <p>This should de-dupe 234</p>
-    </div>
-  )
-}
--- warm-up-fargate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-lc=1
-while [ $lc -le 6 ]
-do
-  now --scope=testing-internal --force &
-  lc=$(( $lc + 1 ))
-done
